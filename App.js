95d06da6718d53f73cd63fc6fe9b87d15a4af9af
emirhankrhn <emirhankrhn07@gmail.com>
2025-11-10 12:31:55 +0300

    feat(part2): Add controlled text input and state for tasks

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StyleSheet, Text, View } from'react-native';
+import { StyleSheet, Text, View, Button , TextInput } from'react-native';
 import { SafeAreaView } from'react-native-safe-area-context';
 export default function App() {
+
+const [enteredTaskText, setEnteredTaskText] = useState('');
+const [tasks, setTasks] = useState([]);
+
+const taskInputHandler = (enteredText) => {
+
+ 
+setEnteredTaskText(enteredText);
+};
+
+function addTaskHandler() {
+
+    if (enteredTaskText.trim().length === 0) {
+        return;
+    }
+    setTasks((currentTasks) => [
+        ...currentTasks,
+        { id: Math.random().toString(), text: enteredTaskText }
+    ]);
+    setEnteredTaskText('');
+
+    
+}
+    
+
+
 return (
 <SafeAreaView style={styles.appContainer}>
 <View style={styles.contentContainer}>
 <Text style={styles.title}>Yapılacaklar Listem</Text>
-{/* Giriş alanı buraya gelecek */}
+<View style={styles.inputContainer}>
+<TextInput
+style={styles.textInput}
+placeholder="Yeni bir görev ekle..."
+onChangeText={taskInputHandler} // State işleyiciye bağlan
+value={enteredTaskText} // Değeri state'e bağla
+/>
+</View>
+<Button title="Ekle" onPress={addTaskHandler} />
+
 {/* Liste alanı buraya gelecek */}
 </View>
 </SafeAreaView>
@@ -28,4 +63,24 @@ fontWeight: 'bold'
 marginBottom: 20,
 textAlign: 'center'
 ,},
+
+inputContainer: {
+flexDirection: 'row',
+justifyContent: 'space-between',
+alignItems: 'center',
+marginBottom: 20,
+borderBottomWidth: 1,
+borderColor: '#ccc',
+paddingBottom: 10,
+},
+textInput: {
+flex: 1, // Mümkün olduğunca çok yer kapla
+borderWidth: 1,
+borderColor: '#ccc'
+,
+padding: 10,
+borderRadius: 6,
+marginRight: 10,
+fontSize: 16,
+},
 });
